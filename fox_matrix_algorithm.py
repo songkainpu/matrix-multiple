adbe3052ba48f6eae67db2b7c1fb1849ca5417b0
songkai <songkai@kuaishou.com>
2023-11-07 03:12:59 +0800

    [add] edit blocks

--- fox_matrix_algorithm.py
+++ fox_matrix_algorithm.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import threading
 import traceback
 from multiprocessing import synchronize, shared_memory
 import argparse
@@ -6,15 +7,19 @@ import time
 import typing
 import logging
 import os
+import functools
 
 import numpy
-from matrix_operation import matrix_multiple
+from matrix_operation import matrix_multiple, matrix_multiple_native
 from multiprocessing.pool import Pool
+
 multiprocessing.set_start_method('fork')
 MATRICES_FILE_FOLDER = "matrices"
 
 logger: logging.Logger = logging.getLogger(name=__name__)
 DEFAULT_SCALES: typing.List[int] = [16, 32, 64, 128, 256, 512, 1024, 2048]
+# DEFAULT_SCALES: typing.List[int] = [2048]
+
 # DEFAULT_SCALES: typing.List[int] = [16]
 SHARE_MEMO_NAME = "matrix calculate"
 
@@ -27,7 +32,7 @@ SPILT_SIZE = 2
 lock = multiprocessing.Lock()
 
 
-def _generate_matrices(scales: typing.Sequence[int] = None,):
+def _generate_matrices(scales: typing.Sequence[int] = None, ):
     if scales is None:
         scales = DEFAULT_SCALES
     for scale in scales:
@@ -219,7 +224,6 @@ def _compute(scale: int, left1: int = 0, right1: int = None, top1: int = 0,
 
 
 def __compute(i: int, j: int, k: int, block_size: int, scale: int):
-
     # f"{i},{j},{k},B"
     # A = matrices_map[scale][0]
     # B = matrices_map[scale][1]
@@ -236,10 +240,12 @@ def __compute(i: int, j: int, k: int, block_size: int, scale: int):
     c_top = b_top
     c_bottom = b_bottom
     shm_A = multiprocessing.shared_memory.SharedMemory(name=f"{i},{j},{k},A")
-    A = numpy.ndarray((a_right - a_left, a_bottom - a_top ), dtype=numpy.int32, buffer=shm_A.buf)
+    A = numpy.ndarray((a_right - a_left, a_bottom - a_top), dtype=numpy.int32, buffer=shm_A.buf)
     shm_B = multiprocessing.shared_memory.SharedMemory(name=f"{i},{j},{k},B")
     B = numpy.ndarray((b_right - b_left, b_bottom - b_top), dtype=numpy.int32, buffer=shm_B.buf)
-    cur_result = matrix_multiple(matrix1=A, matrix2=B)
+    result: numpy.ndarray = numpy.zeros(shape=(A.shape[0], B.shape[1]), dtype=numpy.int32)
+    cur_result = matrix_multiple(matrix1=A, matrix2=B, result=result)
+    # cur_result = fox_compute_synchronize(matrix1=A, matrix2=B,result=result)
     shm_A.close()
     shm_B.close()
     shm = multiprocessing.shared_memory.SharedMemory(name=SHARE_MEMO_NAME)
@@ -249,7 +255,48 @@ def __compute(i: int, j: int, k: int, block_size: int, scale: int):
     shm.close()
 
 
+@functools.cache
+def compute_2_pow(exponent: int) -> int:
+    return 2 ** exponent
 
+
+def fox_compute_synchronize(matrix1: numpy.ndarray, matrix2: numpy.ndarray,
+                            result: numpy.ndarray, sub_matrix_count: int = 2):
+    row1, common, col2 = matrix1.shape[0], matrix1.shape[1], matrix2.shape[1]
+    new_dim = compute_2_pow((max(row1, common, col2) - 1).bit_length())
+    if new_dim != row1 or new_dim != common or new_dim != col2:
+        print(f"不相等")
+        new_matrix = numpy.zeros((new_dim, new_dim))
+        new_matrix[:row1, :common] = matrix1
+        matrix1 = new_matrix
+        new_matrix = numpy.zeros((new_dim, new_dim))
+        new_matrix[:common, :col2] = matrix2
+        matrix2 = new_matrix
+    if new_dim <= sub_matrix_count:
+        return matrix_multiple(matrix1=matrix1[:row1, :common],
+                               matrix2=matrix2[:common, :col2],
+                               result=result)
+    else:
+        block_size = new_dim // sub_matrix_count
+        for k in range(sub_matrix_count):
+            for i in range(sub_matrix_count):
+                for j in range(sub_matrix_count):
+                    a_left = i * block_size
+                    a_right = (i + 1) * block_size
+                    a_top = k * block_size
+                    a_bottom = (k + 1) * block_size
+                    b_left = a_top
+                    b_right = a_bottom
+                    b_top = j * block_size
+                    b_bottom = (j + 1) * block_size
+                    c_left = a_left
+                    c_right = a_right
+                    c_top = b_top
+                    c_bottom = b_bottom
+                    tem_A = matrix1[a_left:a_right, a_top:a_bottom]
+                    tem_B = matrix2[b_left:b_right, b_top:b_bottom]
+                    fox_compute_synchronize(matrix1=tem_A, matrix2=tem_B, result=result[c_left:c_right, c_top:c_bottom])
+        return result
 
 
 def _compute_matrices():
@@ -259,25 +306,15 @@ def _compute_matrices():
         fox_time = []
         for t in range(10):
             pool: multiprocessing.Pool = multiprocessing.Pool(processes=4)
-            start_time = time.time()
-            A = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-A.csv", delimiter=',')
-            B = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-B.csv", delimiter=',')
-            directed_computed = matrix_multiple(matrix1=A,
-                                                matrix2=B)
-            end_time = time.time()
-            directed_time.append(end_time-start_time)
-            # context: multiprocessing.context.ForkContext = multiprocessing.get_context(method='spawn')
+            A = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-A.csv",
+                              delimiter=',', dtype=numpy.int32)
+            B = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-B.csv",
+                              delimiter=',', dtype=numpy.int32)
             result = numpy.zeros(shape=(scale, scale), dtype=numpy.int32)
             shm = shared_memory.SharedMemory(name=SHARE_MEMO_NAME, create=True, size=result.nbytes)
             shared_result_matrix = numpy.ndarray(result.shape, dtype=numpy.int32, buffer=shm.buf)
             shared_result_matrix[:] = result
-            # _compute(scale=scale)
-            # 定义矩阵的规模
-            # block_size = 4  # 块大小
-            # 初始化结果矩阵 C
-            # 定义子矩阵的数量
-            # sub_matrix_count = scale // block_size
-            sub_matrix_count = 4
+            sub_matrix_count = 2
             block_size = scale // sub_matrix_count
             process_list = []
             start_time = time.time()
@@ -294,17 +331,15 @@ def _compute_matrices():
                             b_right = a_bottom
                             b_top = j * block_size
                             b_bottom = (j + 1) * block_size
-                            c_left = a_left
-                            c_right = a_right
-                            c_top = b_top
-                            c_bottom = b_bottom
                             tem_A = A[a_left:a_right, a_top:a_bottom]
                             tem_B = B[b_left:b_right, b_top:b_bottom]
 
-                            tem_A_shm = shared_memory.SharedMemory(name=f"{i},{j},{k},A", create=True, size=tem_A.nbytes)
+                            tem_A_shm = shared_memory.SharedMemory(name=f"{i},{j},{k},A", create=True,
+                                                                   size=tem_A.nbytes)
                             shared_result_matrix_A = numpy.ndarray(tem_A.shape, dtype=numpy.int32, buffer=tem_A_shm.buf)
                             shared_result_matrix_A[:] = tem_A
-                            tem_B_shm = shared_memory.SharedMemory(name=f"{i},{j},{k},B", create=True, size=tem_B.nbytes)
+                            tem_B_shm = shared_memory.SharedMemory(name=f"{i},{j},{k},B", create=True,
+                                                                   size=tem_B.nbytes)
                             shared_result_matrix_B = numpy.ndarray(tem_B.shape, dtype=numpy.int32, buffer=tem_B_shm.buf)
                             shared_result_matrix_B[:] = tem_B
                             shm_list.append(tem_A_shm)
@@ -317,54 +352,42 @@ def _compute_matrices():
                                 "scale": scale,
                             })
                             process_list.append(process_task)
-                        except:
+                        except Exception as e:
                             traceback.print_exc()
                             print(f"i:{i},j:{j},k:{k}")
             for process_task in process_list:
                 process_task.wait()
                 process_task.get()
             end_time = time.time()
-            fox_time.append(end_time-start_time)
+            fox_time.append(end_time - start_time)
             for share_memo in shm_list:
                 share_memo.close()
                 share_memo.unlink()
             main_thread_shm = multiprocessing.shared_memory.SharedMemory(name=SHARE_MEMO_NAME)
             shared_result_matrix = numpy.ndarray((scale, scale), dtype=numpy.int32, buffer=main_thread_shm.buf)
-            # print("Main Thread - Shared Array:", directed_computed)
-
-            # print("Main Thread - Shared Array:", shared_result_matrix)
             shm.close()
             shm.unlink()
-        print(f"scale:{scale}, directed_time:{directed_time}, fox_time:{fox_time}")
-        print(f"scale:{scale}, directed_time avg:{sum(directed_time) / len(directed_time)}, fox_time avg:{sum(fox_time) /len(fox_time)}")
-    # import numpy as np
-    #
-    # # 定义矩阵的规模和块大小
-    # n = 4  # 矩阵的维度
-    # block_size = 2  # 块大小
-    #
-    # # 随机生成两个矩阵 A 和 B
-    # A = np.random.rand(n, n)
-    # B = np.random.rand(n, n)
-    #
-    # # 初始化结果矩阵 C
-    # C = np.zeros((n, n))
-    # # 使用Fox算法进行矩阵乘法
-    # for k in range(n):
-    #     for i in range(n):
-    #         for j in range(n):
-    #             # 计算子块的坐标
-    #             row = (i + k) % n
-    #             col = (j + k) % n
-    #             # 计算局部块的乘积
-    #             local_sum = np.dot(A[i, row::block_size], B[col::block_size, j])
-    #             # 在C中累积局部块的乘积
-    #             C[i, j] += local_sum
-    #
-    # print("Matrix C (Result of Matrix Multiplication):")
-    # print(C)
-    #
-    # pass
+        print(f"scale:{scale}, fox_time:{fox_time}")
+        print(f"scale:{scale},fox_time avg:{sum(fox_time) / len(fox_time)}")
+
+def compute_single() -> None:
+    global DEFAULT_SCALES
+    for scale in DEFAULT_SCALES:
+        synchronize_time = []
+        for t in range(10):
+            A = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-A.csv",
+                              delimiter=',', dtype=numpy.int32)
+            B = numpy.loadtxt(f"{MATRICES_FILE_FOLDER}{os.path.sep}{scale}-{t}-B.csv",
+                              delimiter=',', dtype=numpy.int32)
+            start_time = time.time()
+            result: numpy.ndarray = numpy.zeros_like(A)
+            directed_result = matrix_multiple(matrix1=A, matrix2=B, result=result)
+            end_time = time.time()
+            synchronize_time.append(end_time-start_time)
+        print(f"scale:{scale}, synchronize_time:{synchronize_time}")
+        print(f"scale:{scale}, synchronize_time avg:{sum(synchronize_time) / len(synchronize_time)}")
+
+
 
 
 def __main() -> None:
@@ -376,5 +399,7 @@ def __main() -> None:
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     p = multiprocessing.Process(target=__main)
+    p2 = multiprocessing.Process(target=compute_single)
     p.start()
     p.join()
+
--- matrix_operation.py
+++ matrix_operation.py
@@ -2,18 +2,30 @@ import typing
 import numpy
 
 
+def matrix_multiple_native(matrix1: typing.List[typing.List[int]],
+                           matrix2: typing.List[typing.List[int]],
+                           result:  typing.List[typing.List[int]]) -> typing.List[typing.List[int]]:
+    row1, common, col2 = len(matrix1), len(matrix1[0]), len(matrix2[0])
+    if result is None:
+        result = [([0] * col2)] * row1
+    for i in range(row1):
+        for j in range(col2):
+            for k in range(common):
+                result[i][j] += matrix1[i][ k] * matrix2[k][j]
+    return result
+
 def matrix_multiple(matrix1: typing.Union[typing.List[typing.List[typing.Union[int, float]]], numpy.ndarray],
                     matrix2: typing.Union[typing.List[typing.List[typing.Union[int, float]]], numpy.ndarray],
-                    dtype: numpy.dtype = numpy.int32) \
+                    dtype: numpy.dtype = numpy.int32,
+                    result: typing.Union[typing.List[typing.List[typing.Union[int, float]]], numpy.ndarray] = None) \
         -> numpy.ndarray:
     if isinstance(matrix1, list):
         matrix1 = numpy.asarray(matrix1, dtype=dtype)
     if isinstance(matrix2, list):
         matrix2 = numpy.asarray(matrix2, dtype=dtype)
     row1, common, col2 = matrix1.shape[0], matrix1.shape[1], matrix2.shape[1]
-
-    result = numpy.zeros((row1, col2), dtype=dtype)
-
+    if result is None:
+        result = numpy.zeros((row1, col2), dtype=dtype)
     for i in range(row1):
         for j in range(col2):
             for k in range(common):
