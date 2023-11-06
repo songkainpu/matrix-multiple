14e8005395498fe8ebbbf6affa9e03873917e69b
songkai <songkai@kuaishou.com>
2023-11-07 03:13:56 +0800

    [add] remove redundant codes

--- fox_matrix_algorithm.py
+++ fox_matrix_algorithm.py
@@ -45,184 +45,6 @@ def _generate_matrices(scales: typing.Sequence[int] = None, ):
                           X=result[0], delimiter=',', fmt='%d')
 
 
-def _process_matrix_result(modification_func):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            scale = kwargs.get("scale")
-            left1 = kwargs.get("left1")
-            top1 = kwargs.get("top1")
-            right1 = kwargs.get("right1")
-            bottom1 = kwargs.get("bottom1")
-            left2 = kwargs.get("left2")
-            top2 = kwargs.get("top2")
-            right2 = kwargs.get("right2")
-            bottom2 = kwargs.get("bottom2")
-            if right1 is None:
-                right1 = scale
-                kwargs['right1'] = scale
-            if bottom1 is None:
-                bottom1 = scale
-                kwargs['bottom1'] = scale
-            if right2 is None:
-                right2 = scale
-                kwargs['right2'] = scale
-            if bottom2 is None:
-                bottom2 = scale
-                kwargs['bottom2'] = scale
-            shm1 = multiprocessing.shared_memory.SharedMemory(name=f"{SHARE_MEMO_NAME}:m1")
-            M1: numpy.ndarray = numpy.ndarray((scale, scale), dtype=numpy.int32, buffer=shm1.buf)
-            shm2 = multiprocessing.shared_memory.SharedMemory(name=f"{SHARE_MEMO_NAME}:m2")
-            M2: numpy.ndarray = numpy.ndarray((scale, scale), dtype=numpy.int32, buffer=shm1.buf)
-            numpy.frombuffer()
-            kwargs['M1'] = M1
-            kwargs['M2'] = M2
-            result = func(*args, **kwargs)
-
-
-def _compute(scale: int, left1: int = 0, right1: int = None, top1: int = 0,
-             bottom1: int = None, left2: int = 0, right2: int = None, top2: int = 0,
-             bottom2: int = None, pool: multiprocessing.Pool = None):
-    print(f"scale:{scale}, left1:{left1}, left2:{left2}, right1:{right1}, right2:{right2}")
-    if right1 is None:
-        right1 = scale
-    if bottom1 is None:
-        bottom1 = scale
-    if right2 is None:
-        right2 = scale
-    if bottom2 is None:
-        bottom2 = scale
-
-    cur_scale = bottom1 - top1 + 1
-    A = matrices_map[scale][0]
-    B = matrices_map[scale][1]
-    A = A[left1:right1, top1:bottom1]
-    B = B[left2:right2, top2:bottom2]
-    if not pool:
-        result = matrix_multiple(matrix1=A, matrix2=B)
-        return result
-    pool = multiprocessing.Pool(processes=4)
-    spilt_size = cur_scale // 2
-    Base = {
-        "scale": scale,
-        "pool": pool,
-    }
-    X11 = {
-        "left1": left1,
-        "right1": left1 + spilt_size,
-        "top1": top1,
-        "bottom1": top1 + spilt_size
-    }
-    X12 = {
-        "left1": left1 + spilt_size,
-        "right1": right1,
-        "top1": top1,
-        "bottom1": top1 + spilt_size,
-    }
-    X21 = {
-        "left1": left1,
-        "right1": left1 + spilt_size,
-        "top1": top1 + spilt_size,
-        "bottom1": bottom1
-    }
-    X22 = {
-        "left1": left1 + spilt_size,
-        "right1": right1,
-        "top1": top1 + spilt_size,
-        "bottom1": bottom1
-    }
-
-    Y11 = {
-        "left2": left2,
-        "right2": left2 + spilt_size,
-        "top2": top2,
-        "bottom2": top2 + spilt_size
-    }
-    Y12 = {
-        "left2": left2 + spilt_size,
-        "right2": right2,
-        "top2": top2,
-        "bottom2": top2 + spilt_size,
-    }
-    Y21 = {
-        "left1": left2,
-        "right1": left2 + spilt_size,
-        "top1": top2 + spilt_size,
-        "bottom1": bottom2
-    }
-    Y22 = {
-        "left2": left2 + spilt_size,
-        "right2": right2,
-        "top2": top2 + spilt_size,
-        "bottom2": bottom2
-    }
-    M1_processes_list = [
-        # X11 * Y11
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X11,
-            **Y11,
-            "idx": 0
-        }),
-        # X12 * Y22
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X11,
-            **Y12,
-            "idx": 1
-        }),
-        # X21
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X21,
-            **Y11,
-            "idx": 2
-        }),
-        # X22
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X21,
-            **Y12,
-            "idx": 3
-        })
-    ]
-    M2_processes_list = [
-        # X12 * Y21
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X12,
-            **Y21,
-            "idx": 0
-        }),
-        # X12 * Y22
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X12,
-            **Y22,
-            "idx": 1
-        }),
-        # X22 * Y21
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X22,
-            **Y21,
-            "idx": 2
-        }),
-        # X22 * Y22
-        pool.apply_async(_compute, kwds={
-            **Base,
-            **X22,
-            **Y22,
-            "idx": 3
-        })
-    ]
-    for result in M1_processes_list:
-        result.wait()
-    for result in M2_processes_list:
-        result.wait()
-    # M1_two_dim: typing.List[typing.Optional[numpy.ndarray]] = [result.get() for result in M1_processes_list]
-    # M2_two_dim: typing.List[typing.Optional[numpy.ndarray]] = [result.get() for result in M2_processes_list]
-
-
 def __compute(i: int, j: int, k: int, block_size: int, scale: int):
     # f"{i},{j},{k},B"
     # A = matrices_map[scale][0]
@@ -370,6 +192,7 @@ def _compute_matrices():
         print(f"scale:{scale}, fox_time:{fox_time}")
         print(f"scale:{scale},fox_time avg:{sum(fox_time) / len(fox_time)}")
 
+
 def compute_single() -> None:
     global DEFAULT_SCALES
     for scale in DEFAULT_SCALES:
@@ -383,11 +206,9 @@ def compute_single() -> None:
             result: numpy.ndarray = numpy.zeros_like(A)
             directed_result = matrix_multiple(matrix1=A, matrix2=B, result=result)
             end_time = time.time()
-            synchronize_time.append(end_time-start_time)
+            synchronize_time.append(end_time - start_time)
         print(f"scale:{scale}, synchronize_time:{synchronize_time}")
         print(f"scale:{scale}, synchronize_time avg:{sum(synchronize_time) / len(synchronize_time)}")
-
-
 
 
 def __main() -> None:
@@ -402,4 +223,3 @@ if __name__ == "__main__":
     p2 = multiprocessing.Process(target=compute_single)
     p.start()
     p.join()
-
